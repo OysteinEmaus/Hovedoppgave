aca02482bf2f75254174e3cf98da225546ac1d1e
oysteinemaus <oysteine@gmail.com>
2025-05-02 22:50:13 +0200

    Add project files.

---
+++ Hovedoppgave_2.Server/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hovedoppgave_2.Server.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } = "User"; // Default rolle
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Report> Reports { get; set; }
+    }
+}
---
+++ Hovedoppgave_2.Server/README.txt
@@ -0,0 +1,13 @@
+Nødvendige nuggetpakker:
+
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer 
+dotnet add package Microsoft.EntityFrameworkCore 
+dotnet add package Microsoft.EntityFrameworkCore.Design 
+dotnet add package Pomelo.EntityFrameworkCore.MySql 
+dotnet add package Microsoft.EntityFrameworkCore.Tools 
+dotnet add package BCrypt.Net-Next 
+dotnet add package Microsoft.IdentityModel.Tokens 
+dotnet add package System.IdentityModel.Tokens.Jwt
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer --version 6.0.0 måtte bruke en spesfikk versjon for at det skulle funke
+dotnet add package Swashbuckle.AspNetCore
+dotnet add package Microsoft.EntityFrameworkCore.Relational
